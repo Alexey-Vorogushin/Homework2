297ae6862b03a94391e676b6b3aa49bdd0f78d07
Alexey-Vorogushin <alex_dernond@mail.ru>
2023-12-19 14:40:53 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+        dog = dog + 2;
+        cat = cat + 2;
+        paper = paper + 2;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
