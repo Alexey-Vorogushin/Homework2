4ff7a960b5621ecbaa914716653e5d3bf1ca9c81
Alexey-Vorogushin <alex_dernond@mail.ru>
2023-12-19 15:11:24 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+        var manOne = 78.2;
+        var manTwo = 82.7;
+        System.out.println("Масса первого боксера= " + manOne + " Масса второго боксера= " + manTwo);
+        var result = manOne + manTwo;
+        System.out.println("Общая масса двух боксеров равна " + result);
+        var resultTwo = manTwo - manOne;
+        System.out.println("Разница в весе между массами бойцов равна " + resultTwo + " кг");
 
     }
 }
